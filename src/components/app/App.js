d2103700ff5ff97c172d6c159321d3173d43522a
Nazar <nz.shevchuk.2002@gmail.com>
2023-02-28 18:01:03 +0200

    dispatch + bootstrap

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,36 @@
-import './app.css';
+import './app.scss';
 import { createStore } from 'redux';
+import {Button} from 'react-bootstrap';
+import {reducer} from '../reducer.js';
+import {inc, dec} from '../actions.js'
 
-const reducer = (state = 0, action) => {
-  switch(action.type){
-    case 'INC': return state + 1;
-    case 'DEC': return state - 1;
-    default: return state;
-  };
-};
 
-const store = createStore(reducer)
+const {dispatch, subscribe, getState} = createStore(reducer)
 
+const incDispatch = () => dispatch(inc())
+const decDispatch = () => dispatch(dec())
+
+subscribe(() => {
+  updateDiv();
+})
 
 const  App = () => {
+
   return (
     <div className="App">
-      <button onClick={() => store.dispatch({type: 'INC'})}>INC</button>
-      {store.getState()}
-      <button onClick={() => store.dispatch({type: 'DEC'})}>DEC</button>
+      <Button onClick={incDispatch} variant="dark">+</Button>
+      <Button onClick={decDispatch} variant="dark">-</Button>
     </div>
   );
 }
 
+const div = document.getElementById('counter')
+
+function updateDiv(){
+  div.textContent = getState();
+}
+
+updateDiv();
 
 
 export default App;
