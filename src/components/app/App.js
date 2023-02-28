1c01861a4b48ced0c27bc1d6fa0c726ad3868b61
Nazar <nz.shevchuk.2002@gmail.com>
2023-02-28 16:19:28 +0200

    install redux + react-bootstrap

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,27 @@
 import './app.css';
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch(action.type){
+    case 'INC': return state + 1;
+    case 'DEC': return state - 1;
+    default: return state;
+  };
+};
+
+const store = createStore(reducer)
+
 
 const  App = () => {
   return (
     <div className="App">
-
+      <button onClick={() => store.dispatch({type: 'INC'})}>INC</button>
+      {store.getState()}
+      <button onClick={() => store.dispatch({type: 'DEC'})}>DEC</button>
     </div>
   );
 }
+
+
 
 export default App;
