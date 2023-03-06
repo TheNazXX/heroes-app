31dd090f136ee69bffba7e6929fdb0bf7f0adbe3
Nazar <nz.shevchuk.2002@gmail.com>
2023-03-06 18:49:16 +0200

    redux toolkit action-creater

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
     request("http://localhost:3001/heroes")
@@ -5,62 +7,15 @@ export const fetchHeroes = (request) => (dispatch) => {
         .catch(() => dispatch(heroesFetchingError))
 }
 
+export const heroesFetching = createAction('HEROES_FETCHING');
+export const heroesFetched = createAction('HEROES_FETCHED');
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
+export const heroDeleted = createAction('HERO_DELETED');
+export const addHero = createAction('HERO_ADD');
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+export const filtersFetching = createAction('FILTERS_FETCHING');
+export const filtersFetched = createAction('FILTERS_FETCHED');
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
+export const changeActiveFilter = createAction('CHANGE_ACTIVE_FILTER');
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
-
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
-
-export const addHero = (obj) => {
-    return {
-        type: 'HERO_ADD',
-        payload: obj
-    }
-}
-
-export const filtersFetching = () => {
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
-
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
-
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
-
-export const changeActiveFilter = (filter) => {
-    return {
-        type: 'CHANGE_ACTIVE_FILTER',
-        payload: filter
-    }
-}
